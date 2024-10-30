a06c5534b4a90eadc7484505c40c576f0733cd82
Vfedorenko72 <v.fedorenko@sh.dunice.net>
2024-10-30 17:50:28 +0300

    1

---
+++ src/main/java/main/CoffeeMachine.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class CoffeeMachine {
+    static String whitespace = " ";
+    public static void main(String[] args) {
+        ;
+        Scanner sc = new Scanner(System.in);
+        Launch session = new Launch();
+        session.indent(30);
+        session.border();
+        System.out.println(whitespace.repeat(5) + "Кнопка включателя кофемашины (1/0): ");
+        session.border();
+
+        session.launch(sc.nextInt());
+
+
+
+
+    }
+
+}
